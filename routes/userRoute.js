30d76afd73ef63d526ce5deec251b389947f987f
Mehmet Maytahan <maytahan71@gmail.com>
2023-02-08 21:39:57 +0300

    kisiye ozel icerik eklendi

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,12 +2,14 @@ const express = require("express");
 const {
   createUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  getDashboardPage
 } = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/signup", createUser); // http://localhost:3000/users/signup
 router.post("/login", loginUser); // http://localhost:3000/users/login
 router.get("/logout", logoutUser); // http://localhost:3000/users/logout
+router.get("/dashboard", getDashboardPage); // http://localhost:3000/users/logout
 
 module.exports = router;
