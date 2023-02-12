fbe74d4b338b8fcab241abd294bd67091a248262
Mehmet Maytahan <maytahan71@gmail.com>
2023-02-12 14:32:20 +0300

    uygulama arayuzunden kurs olusturulma yapildi.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -4,10 +4,7 @@ const Course = require("../models/Course");
 exports.createCourse = async (req, res) => {
   try {
     const course = await Course.create(req.body);
-    res.status(201).json({
-      status: "success",
-      course
-    });
+    res.status(201).redirect("/courses");
   } catch (error) {
     res.status(400).json({
       status: "fail"
